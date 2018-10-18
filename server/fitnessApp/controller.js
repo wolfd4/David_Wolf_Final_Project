47e95dea37739714c43078f93c1b0d23e9991dbf
David Wolf <wolfd4@hawkmail.newpaltz.edu>
2018-10-17 22:14:29 -0400

    Added most functionallity to the the app

--- server/fitnessApp/controller.js
+++ server/fitnessApp/controller.js
@@ -1,19 +1,66 @@
 const express = require('express');
 const {fitnessApp, User} = require('./model');
-
 var fitapp = new fitnessApp();
 const app = express.Router();
 
+//shows all users
 app.get("/", function(req, res){
     res.send(fitapp.users);
 });
 
-//adds a user
+//adds a user to the fitapp
 app.post('/users', (req, res) => {
     const user = new User(req.body.name, fitapp.users.length+1, req.body.age, req.body.height, req.body.weight, req.body.activity);
     fitapp.users.push(user);
     res.send(user);
 })
 
+//retrieves user based on name
+app.get('/users/:name', (req, res) => {
+    const username =  fitapp.users.find(n => n.name === String(req.params.name));
+    if(!username){
+        res.status(404).send('User not found');
+    };
+    res.send(username);
+})
 
+//adds a friend to the user's friends list
+app.post('/addFriend/:name', (req, res) => {
+    const friend = fitapp.users.find(n => n.name === String(req.params.name));
+    if(!friend){
+        res.status(404).send('Friend not found');
+    };
+    const CurrentUser = fitapp.users.find(n => n.id === 1);
+    if(friend.id === CurrentUser.id){
+        res.status(404).send('Cannot add yourself as a friend')
+    };
+    CurrentUser.friendsList.push(friend);
+    res.send(CurrentUser)
+});
+
+//dispays users aggergated data
+
+//Adds a workout to workout completed
+app.post('/addWorkout', (req, res) => {
+    var workout = req.body.workout;
+    const CurrentUser = fitapp.users.find(n => n.id === 1);
+    CurrentUser.workoutsDone.push(workout);
+    res.send(CurrentUser);
+});
+
+//Adds a calories to intake
+app.post('/setIntake', (req, res) => {
+    const CurrentUser = fitapp.users.find(n => n.id === 1);
+    var inTake = req.body.inTake;
+    CurrentUser.changeInTake(inTake);
+    res.send(CurrentUser);
+});
+//gets BMI
+app.post('/getBMI', (req, res) => {
+    const CurrentUser = fitapp.users.find(n => n.id === 1);
+    var BMI = CurrentUser.BMI;
+    res.send(`${BMI}`)
+});
+
+//display users information
 module.exports = app;
--- server/fitnessApp/model.js
+++ server/fitnessApp/model.js
@@ -13,29 +13,28 @@ class User{
         this.weight = weight;
         this.activity = activity;
         this.workoutsDone = [];
-    }
-}
-
-/*class User{
-    constructor(name, id, age, height, weight, activity){
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.activity = activity;
-        this.workoutsDone = [];
+        this.friendsList = [];
+        this.calorieInTake = 0;
+        this.changeInTake = function(inTake){
+            this.calorieInTake = inTake;
+        }
     };
-    getBMI(){
-        return (this.weight/Math.pow(this.height))
-    };
-    getBMR(){
-        return(66.5 + (13.75 * this.weight) + (5.003 * this.height) - (6.755 * this.age))
-    }
-    getTDEE(){
-        return this.getBMR * this.activity
-    };
-};*/
+    get BMI(){
+        var BMI = (703 * (this.weight / (Math.pow(this.height, 2))));
+        if(BMI < 18.5){
+            return "You are underweight"
+        }
+        if(BMI>=18.5 && BMI <= 24.9){
+            return "You are a healthy weight"
+        }
+        if(BMI>=25.0 && BMI <= 29.9){
+            return "You are overweight"
+        }
+        if(BMI>=30.0){
+            return "You are obese"
+        }
+     }
+};
 
 module.exports = {
     fitnessApp, User
